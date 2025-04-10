133a456369b215814a2a0f928d2ab84bca9b2d82
Daniel Wendland <danielwendland@Daniels-MacBook-Air.local>
2025-04-10 00:27:09 -0500

    restored full volume script

--- volume_of_sphere.py
+++ volume_of_sphere.py
@@ -1,2 +1,11 @@
 # Edited by Dan for Git Exercise 1
 
+import math
+
+def calculate_volume_of_sphere(radius):
+    return (4/3) * math.pi * radius**3
+
+radii = [30, 40]
+for r in radii:
+    volume = calculate_volume_of_sphere(r)
+    print("Radius:", r, "| Volume:", round(volume, 2))
